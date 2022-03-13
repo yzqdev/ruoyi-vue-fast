f972e80237158d7ea58e356429efe61a707defbe
yzqdev <yzqdev@outlook.com>
2022-03-13 17:42:08 +0800

    update

---
+++ ruoyi-ui-vue3/vite.config.js
@@ -0,0 +1,19 @@
+// vite.config.js
+import { createVuePlugin } from 'vite-plugin-vue2'
+import path from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  server: {
+    port: 80
+  },
+  plugins: [
+    createVuePlugin(/* options */{ jsx: true })
+  ], resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+    alias: {
+      // vue2项目别名一般都是@，vue3中一般使用/@/, 为方便使用
+      '@': path.resolve('src')
+    }
+  }
+})
